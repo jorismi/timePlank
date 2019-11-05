f8dfd9b739873f73da7314e8889d013dc4cf6471
Your Name <you@example.com>
2019-11-05 20:28:00 +0100

    adding ngModel

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,17 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  private count=5;
+  private minutes = 0;
+  private seconds = 0;
 
+  constructor(public navCtrl: NavController) {
+    
   }
 
+  changeWorkInterval(value: number) {
+    //let tmpSeconds = this.seconds+value;
+    //this.seconds=tmpSeconds.toFixed(2);
+    this.seconds+=value;
+  }  
 }
